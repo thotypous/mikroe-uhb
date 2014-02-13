f3745f460511bfc09bf5870873bcda19deac215c
Paulo Matias <paulo.matias@usp.br>
2014-02-13 11:12:19 -0200

    Prepare setup for supporting other OS
    Operating systems other than Linux will be supported
    in near future by using the cython-hidapi library.

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
+import sys
+
+install_requires = []
+if sys.platform.startswith("linux"):
+    install_requires += ["pyudev>=0.13"]
+else:
+    install_requires += ["hidapi>=0.7.99"]
+
 setup(
     name = "mikroe-uhb",
     version = "0.1",
     packages = find_packages(exclude=["*.tests"]),
-    install_requires = ["pyudev>=0.13"],
+    install_requires = install_requires,
     test_suite = "mikroeuhb.tests",
     scripts = ["mikroe-uhb"],
     use_2to3 = True,
